361858ad057ab8ff8b001e50d87690ed7d3fad99
gowrishankar95 <gowrishankar@hsenidmobile.com>
2019-07-25 17:04:34 +0530

    java docs for UserRepository interface are added

--- UserManagement/src/main/java/com/hms/management/dao/UserRepository.java
+++ UserManagement/src/main/java/com/hms/management/dao/UserRepository.java
@@ -5,16 +5,36 @@ import com.hms.management.User;
 import java.util.List;
 
 public interface UserRepository {
-    //todo : Add java doc
 
+    /**
+     * This is the method to be used to create
+     * user.
+     */
     User create(User user);
 
+    /**
+     * This is the method to be used to update
+     * password of a user. should provide userId,
+     * old password and new password
+     */
     String updatePassword(int UserId, String oldPassword, String newPassword);
 
+    /**
+     * This is the method to be used to delete
+     * user. should provide userID
+     */
     User deleteUser(int id);
 
+    /**
+     * This is the method to be used to view
+     * all users.
+     */
     List<User> viewAllUsers();
 
+    /**
+     * This is the method to be used to get
+     * a user with perticular userId
+     */
     User getUser(int userId);
 
 
